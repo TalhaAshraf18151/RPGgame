b1fdd505358b027d6cec6720c32fea09b2ba86e4
TA18151 <ta18151@student.phs.school.nz>
2020-11-05 12:22:34 +1300

    Update RPG_game_new.py

--- RPG_game_new.py
+++ RPG_game_new.py
@@ -30,10 +30,10 @@ Commands:''' + commands.__str__()
 def Status():
     # print the player's current status
     print('\n----------------------------')
-    print('You are in ' + currentRoom)
+    print('You are in ' + current_room)
     # print the current interactable
-    if 'intractable' in rooms[currentRoom]:
-        print('You see ' + rooms[currentRoom]['intractable'])
+    if 'intractable' in rooms[current_room]:
+        print('You see ' + rooms[current_room]['intractable'] + "\n")
 
 
 #function for recording user's name and greeting
@@ -41,8 +41,8 @@ name = ""
 def namefunc():
     global name
     print('What is your name?')
-    name += input(">")
-    print('Nice to meet you ' + name)
+    name += input("\n>")
+    print('\nNice to meet you ' + name)
 
 
 
@@ -69,12 +69,14 @@ scroll_of_truth = '''
 -Each New Zealander consumes approximately 31 kg of plastic packaging every single year
 -Each New Zealander recycles approximately 5.58kgs of plastic packaging every single year
 -Recycling a single plastic bottle can conserve enough energy to power a 60W bulb for 3 hours
+-New Zealand waterways carry approximately 4,000,000 tonnes of plastic to sea
+-Plastic waste leeches toxic chemicals into our waterways killing animals and putting humans at risk
 
 Those are just some facts but I would like help you think deeper into the problem. We like to blame  people like you and
 I for disposing of their plastic irresponsibly  but why does nobody talk about how manufacturers continue to use single 
-use plastic packaging while being well aware of its impact. I urge you to not only dispose of plastic responsibly so 
-that it does not cause any more harm to the environment but also to raise your voice and support to force companies to 
-find an alternate to plastic and save our planet.
+use plastic packaging while being well aware of its impact. I urge you to not only dispose of plastic responsibly so
+that it does not cause any more harm to the environment but also to raise your voice to spread awareness about plastic 
+so other people can play their role as well.
 '''
 
 
@@ -89,19 +91,28 @@ def intractScroll_room():
 
 
 
+ending_messege = '''Thank you so much for playing my game. I hope that it inspires you to think about the way you throw 
+away your plastic and the consequences of your actions'''
+
+
 
 #score checker function
+#will end game if score is highest (5)
+#will deposit person back into Quiz_Room if not 5
 def score_check():
-    if correct_answers == 4:
+    if correct_answers == 5:
         print("###################"
-              "GOOD JOB YOU WON !!"
+              " GOOD JOB YOU WON !! "
               "###################")
-        time.sleep(10)
-        print("Thanks for playing my game. I hope you learnt something today:)")
+        time.sleep(5)
+        for letter in ending_messege:
+            sys.stdout.write(letter)
+            sys.stdout.flush()
+            time.sleep(0.03)
         exit()
-    elif correct_answers == 3:
+    elif correct_answers == 4:
         print("Try again. You're so close you got 3 right")
-    elif correct_answers < 3:
+    elif correct_answers < 4:
         print("Try again you got " + correct_answers.__str__() + ' correct')
 
 
@@ -128,7 +139,7 @@ def intractQuiz_room():
     print('current score is ' + correct_answers.__str__())
     print("\n")
 
-    q2 = input("\nQ2) How much plastic does the average New Zealander recycle? \na7.3 kg\nb)5.58 kg\nc)6.9 kg\n>")
+    q2 = input("\nQ2) How much plastic does the average New Zealander recycle? \na) 7.3 kg\nb) 5.58 kg\nc) 6.9 kg\n>")
     if q2.lower() == "b":
         print("Good job correct answer\n")
         correct_answers += 1
@@ -137,29 +148,30 @@ def intractQuiz_room():
     print('current score is ' + correct_answers.__str__())
     print("\n")
 
-    q3 = input("How long can it take plastic to break down? \na)9000 years\nb)100 years\nc)1000 years\n>")
+    q3 = input("How long can it take plastic to break down? \na) 9000 years\nb) 100 years\nc) 1000 years\n>")
     if q3.lower() == "c":
         print("Good job correct answer\n")
         correct_answers += 1
     else:
-        print("Oops, seems like you are incorrect the correct answer is actually \nc)1000 years\n")
+        print("Oops, seems like you are incorrect the correct answer is actually \nc) 1000 years\n")
     print('current score is ' + correct_answers.__str__())
     print("\n")
 
-    q4 = input("How much plastic does the each New Zealander use in a year?\na)72 Kg\nb)14 Kg\nc)31 Kg")
+    q4 = input("How much plastic does the each New Zealander use in a year?\na) 72 Kg\nb) 14 Kg\nc) 31\n>")
     if q4.lower() =="c":
         print("Good job correct answer\n")
         correct_answers += 1
     else:
-        print("Oops, seems like you are incorrect the correct answer is actually \nc)31 Kg\n")
+        print("Oops, seems like you are incorrect the correct answer is actually \nc) 31 Kg\n")
+    print('current score is ' + correct_answers.__str__())
+    print("\n")
 
-
-    q5 = input("Recycling a single plastic bottle conserves enough energy to power a 60W bulb for how long?\na)30 mins\nb)60 mins\nc)20 mins\n>")
+    q5 = input("Recycling a single plastic bottle conserves enough energy to power a 60W bulb for how long?\na) 30 mins\nb) 60 mins\nc) 20 mins\n>")
     if q5.lower() == "a":
         print("Good job correct answer\n")
         correct_answers += 1
     else:
-        print("Oops, seems like you are incorrect the correct answer is actually \na)30 mins\n")
+        print("Oops, seems like you are incorrect the correct answer is actually \na) 30 mins\n")
     t1 = time.time() - t0
     print("It only took you " + round(t1).__str__() + " seconds")
     score_check()
@@ -183,7 +195,7 @@ rooms = dict(Spawn_room={'up': 'Scroll_room',
                                                        })
 
 # start the player in the Hall
-currentRoom = 'Spawn_room'
+current_room = 'Spawn_room'
 
 opening_screen()
 
@@ -191,7 +203,7 @@ opening_screen()
 while True:
 
     Status()
-
+    
     # get the player's next 'move'
     input_user = ''
     while input_user == '':
@@ -202,9 +214,9 @@ while True:
     #check if input is a direction
     if input_user in directions:
         # check that they are allowed wherever they want to go
-        if input_user in rooms[currentRoom]:
+        if input_user in rooms[current_room]:
             # set the current room to the new room
-            currentRoom = rooms[currentRoom][input_user]
+            current_room = rooms[current_room][input_user]
         elif input_user not in directions:
             print('that\'s not a direction I recognise try ' + directions.__str__())
         # there is no door (link) to the new room
@@ -212,7 +224,7 @@ while True:
             print('You can\'t go that way!')
     # calls function for room
     elif input_user in ['interact', 'read', 'pick']:
-        callable(rooms[currentRoom]['function']())
+        callable(rooms[current_room]['function']())
     # if user asks for help
     elif input_user == 'help':
         help_screen()
